9b577743ed09c6885fe7f2e0821b3bb77c72e46e
Brandon Perry <bperry.volatile@gmail.com>
2015-07-05 09:48:34 -0500

    first

---
+++ LegacyEventLog.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace evtsharp
+{
+	public class LegacyEventLog
+	{
+		public LegacyEventLog (string filename)
+		{
+			using (FileStream stream = File.OpenRead(filename))
+			{
+				using (BinaryReader reader = new BinaryReader(stream))
+				{
+					uint headerLength = reader.ReadUInt32();
+					uint signature = reader.ReadUInt32();
+					reader.BaseStream.Position += 12;
+					uint footerOffset  = reader.ReadUInt32();
+					uint nextIndex = reader.ReadUInt32();
+					uint fileLength = reader.ReadUInt32();
+					reader.BaseStream.Position += 12;
+					uint endHeaderLength = reader.ReadUInt32();
+					int totalEvents = (int)nextIndex - 1;
+					
+					reader.BaseStream.Position = footerOffset;
+					
+					this.Items = new List<LegacyLogItem>();
+					for (int c = 0; c <= totalEvents; c++)
+						this.Items.Add(new LegacyLogItem(reader));
+				}
+			}
+		}
+		
+		public List<LegacyLogItem> Items { get; set; }
+	}
+}
+
